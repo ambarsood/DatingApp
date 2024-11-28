9bef0675102cf95b9ae7581ecab6385577650bdd
ambarsood <104883218+ambarsood@users.noreply.github.com>
2024-11-29 00:28:35 +0530

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    //private readonly DataContext _context = context; //do not need this with primary contructor
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if(user==null) return NotFound();
+        return user;
+    }
+}
