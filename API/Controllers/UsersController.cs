1078c580c5f1278c3034a3719f7be2ee11f1a6aa
ambarsood <104883218+ambarsood@users.noreply.github.com>
2024-12-06 03:07:36 +0530

    authorization, authentication and identity and cleanings : S4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,16 +1,16 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController(DataContext context) : ControllerBase
+
+public class UsersController(DataContext context) : BaseApiController
 {
     //private readonly DataContext _context = context; //do not need this with primary contructor
-
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -18,6 +18,8 @@ public class UsersController(DataContext context) : ControllerBase
 
         return users;
     }
+
+    [Authorize]
     [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
